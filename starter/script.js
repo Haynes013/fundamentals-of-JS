9bd211d8c095d4f5bb599be378fd647486be6074
Mark Haynes Jr <mhaynes031395@gmail.com>
2021-05-20 23:04:23 -0400

    values

---
+++ starter/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+console.log(3 + 10 + (2021-1989))
+console.log(js)
+
+let firstName = 'Mark'
+
+console.log(firstName)
+
+let country = 'USA';
+let continent = 'North America';
+let population = '18 Million';
+
+console.log(country)
+console.log(continent)
+console.log(population)
