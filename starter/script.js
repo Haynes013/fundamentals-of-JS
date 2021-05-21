450b2de6fe27df13305af2df6c7011c643d3d0eb
Mark Haynes Jr <mhaynes031395@gmail.com>
2021-05-20 23:24:52 -0400

    data types

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,5 @@
+// Values
+
 let js = "amazing";
 console.log(3 + 10 + (2021-1989))
 console.log(js)
@@ -13,3 +15,9 @@ let population = '18 Million';
 console.log(country)
 console.log(continent)
 console.log(population)
+
+// Data Types
+// Numbers: Floating point numbers, Used for decimals and integers
+// String: Sequence of characters, used for text
+// Boolean: Logical type that can only be true or false, Used for making decisions
+
